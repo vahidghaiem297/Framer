df1c81953918ddf97a03c3c8f857c69c26c7c6b8
Vahid ghaiem <89352414+vahidghaiem297@users.noreply.github.com>
2024-08-02 11:50:57 +0330

    Add files via upload

---
+++ Framer/masked-cursor/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MaskedCursor from './components/MaskCursor/MaskedCursor.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <MaskedCursor />
+    </div>
+  );
+}
+
+export default App;
